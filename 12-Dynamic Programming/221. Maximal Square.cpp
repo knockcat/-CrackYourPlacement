3e1b7568ad851059f128fbca6ca9476911b71184
VISHAL JOSHI <85362504+knockcat@users.noreply.github.com>
2023-08-12 14:22:02 +0530

    Maximal Square

---
+++ 12-Dynamic Programming/221. Maximal Square.cpp
@@ -0,0 +1,37 @@
+// Time Complexity -> O(n^2)
+// Space Complexity -> O(n)
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        
+        int n = matrix.size(), m = matrix[0].size();
+        
+        vector<int> curr(m, 0), prev(m , 0);
+        
+        int ans = 0;
+        
+        for(int i = 0;  i < n; ++i)
+        {
+            for(int j = 0; j < m; ++j)
+            {
+                if(i == 0 or j == 0)
+                    curr[j] = matrix[i][j] - 48;
+                
+                if(i > 0 and j > 0 and matrix[i][j] != '0')
+                {
+                    int leftDiagonal = prev[j-1];
+                    int bottomLeft = curr[j-1];
+                    int topRight = prev[j];
+                    
+                    curr[j] = min({leftDiagonal, bottomLeft, topRight}) + 1;
+                }
+                ans = max(ans,curr[j]);
+            }
+            prev = curr;
+        }
+        
+        return ans * ans;
+        
+    }
+};
